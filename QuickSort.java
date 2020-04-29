722088530b74d8f63e106bc35d61c80b3fc9c0da
jakobOrel <55172188+jakoborel@users.noreply.github.com>
2020-04-28 23:17:33 -0500

    Update QuickSort.java
    It seems like changing line 6 to "r" instead of "r-1" fixed the issue.

--- QuickSort.java
+++ QuickSort.java
@@ -3,7 +3,7 @@ public class QuickSort
     private static int partition(int intArray[], int p, int r){
         int x = intArray[r];
         int i = p - 1;
-        for(int j = p; j < r-1; j++){
+        for(int j = p; j < r; j++){
             if(intArray[j] < x){
                 i++;
                 int temp = intArray[i];
@@ -15,7 +15,7 @@ public class QuickSort
         intArray[i + 1] = x;
         return i + 1;
     }
-    
+
     public static void quickSort(int intArray[], int p, int r){
         if(p<r){
             int pivot = partition(intArray, p, r);
@@ -23,18 +23,18 @@ public class QuickSort
             quickSort(intArray, pivot + 1, r);
         }
     }
-    
+
     private static void printArray(int intArray[]){
        for(int i = 0; i < intArray.length; i++)
             System.out.println(intArray[i]);
        System.out.println();
     }
-    
+
     public static void main(String args[]){
         int[] intArray = {30,1,6,0,10};
-        
+
         quickSort(intArray, 0, intArray.length - 1);
-        
+
         printArray(intArray);
     }
 }
